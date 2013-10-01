0362952b7423912fdee2241874b30318b59cc20c
Fagner Gomes <fagnerg@sony13.(none)>
2013-10-01 17:49:54 -0300

    adicionado testes para o servidor, e refatoracao

---
+++ src/Arduino/Testes/post.lua
@@ -0,0 +1,64 @@
+
+local post = {}
+local response = nil
+local code = nil
+local http = require("socket.http")
+local ltn12 = require("ltn12")
+post.morpheus = "http://localhost"
+post.port = 5000
+post.endpoint = "/control.html"
+
+
+local sink = {}
+local loading = nil
+function sink.table(t)
+	t = t or {}
+	local f = function(chunk, err)
+		if chunk then
+			if (loading) then loading() end
+			table.insert(t, chunk)
+		end
+		return 1
+	end
+	return f, t
+end
+
+post.POST = function (reqbody, fLoading)
+	loading = fLoading
+	response = {}
+	local a
+	a, code = http.request {
+	   method = "POST",
+	   url = post.urlDefault()..reqbody ,
+	   headers= {
+				["Content-Type"] = "application/json",
+				["Content-Length"] = string.len(reqbody),
+	   },
+	   source = ltn12.source.string(reqbody),
+	   sink = sink.table(response)
+	}
+	return table.concat(response), code
+end 
+
+post.GET = function (url)
+
+ loading = fLoading
+	response = {}
+	local a
+	a, code = http.request {
+	   method = "GET",
+	   url = post.urlDefault() ,
+	   headers= {
+				["Content-Type"] = "application/json",
+				
+	   },
+	   sink = sink.table(response)
+	}
+	return table.concat(response), code
+
+end
+post.urlDefault = function()
+return post.morpheus..":".. post.port.. post.endpoint 
+end
+
+return post
