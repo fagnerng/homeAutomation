fe28d32e863257e8c6dc29d12bd4484aaa78828a
alexnum <alexnum@gmail.com>
2013-10-03 18:35:20 -0300

    Telas e javascript[A

--- src/Node/server.js
+++ src/Node/server.js
@@ -5,6 +5,7 @@ var users = [];
 var devices = [];
 var jsonDataBase;
 
+var autLogin = ""
 console.log("Server Initiated");
 
 app.use(express.methodOverride());
@@ -44,10 +45,7 @@ function createNewUser(name, deviceIDS){
 	console.log(jsonDataBase);
 }
 
-
-app.get('/getusers', function(req, res){
-	res.type('text/plain');
-	
+function fillUsers(){
 	var jsonData = require('./BD/database.json');
 	var usersLen = jsonData.alldata.users.length
 	
@@ -56,25 +54,87 @@ app.get('/getusers', function(req, res){
 	    users[i] = jsonData.alldata.users[i].name;
 	}
 	
-	jsonDataBase = jsonData; //updating the jsonText pra poder altera-lo adicionando novos dados
+	jsonDataBase = jsonData; 
+}
+
+
+app.get('/getusers', function(req, res){
+	fillUsers();
+	res.type('text/plain');
+	
+	//updating the jsonText pra poder altera-lo adicionando novos dados
 	//~ res.send(jsonData);
 	res.send(users.toString());
 });
 
-app.get('/getdevices', function(req, res){
-	res.type('text/plain');
-	
+app.post('/checkLogin', function(req, res){
+	fillUsers();
 	var jsonData = require('./BD/database.json');
-	var devicesLen = jsonData.alldata.devices.length
-	
-	for (var i=0; i < devicesLen; i++) {
-	    console.log(jsonData.alldata.devices[i].name);
-	    devices[i] = jsonData.alldata.devices[i].name;
+	var usersLen = jsonData.alldata.users.length
+
+
+	if (req.param('name') != undefined && req.param('name') != ""){ //testei enviando requisicao POST a "http://localhost:9000/adduser?" e a "http://localhost:9000/adduser?name="
+		//~ users.push(req.param('name'));
+		userName=true;
 	}
-	
-	//~ res.send(jsonData);
-	res.send(devices.toString());
+	var userLogin = req.param('name');
+	var autenticado = false;
+	for(var i=0; i<usersLen;i++){
+		console.log(users[i] + " " + userLogin );
+		if(users[i] == userLogin){
+			console.log("Condicao");
+			autenticado = true;
+		}
+	}
+	console.log("entrou no post")
+	console.log(autenticado)
+	if(autenticado){
+		autLogin = "Ok!";
+	}else{
+		autLogin = "No"
+	}
 });
+
+app.get('/devices', function(req, res){
+	var id = req.param('id');
+	var status = req.param('status');
+	switchPower(id,status);
+});
+
+
+app.get('/checkLogin', function(req, res){
+	fillUsers();
+	res.send(autLogin);
+});
+
+
+app.get('/getdevices', function(req, res){
+	res.type('text/plain');	
+	var JsonDevices = []
+	var jsonData = require('./BD/database.json');
+	var DevicesLen = jsonData.alldata.devices.length;
+
+
+	for (var i=0; i < DevicesLen; i++) {
+	    console.log(jsonData.alldata.devices[i].name);
+	    JsonDevices[i] = jsonData.alldata.devices[i].name;
+	}
+	res.send(JsonDevices.toString());
+});
+
+//New
+app.post('/deleteDevices', function(req, res){
+	var devices = "";
+	var usuario = req.param('user'); // O usuario que tera os dispositivos selecionados
+	var dispositivos = req.param('selected'); // O id dos dispositivos a serem deletados
+});
+
+//New 
+app.post('/deleteUser', function(req, res){
+	console.log(req.param('user'));
+	var usuario = req.param('user'); // O usuario que tera os dispositivos selecionados
+});
+
 
 app.post('/adduser', function(req, res){
 	var userName = false;
@@ -89,11 +149,62 @@ app.post('/adduser', function(req, res){
 		userDevice=true;
 	}
 	
-	if (userName && userDevice){
+	if (userName){
 		console.log("emtroooou1");
-		createNewUser(req.param('name'), req.param('devices'));
+		createNewUser(req.param('name'), "1");
 	}
 });
+
+var http = require('http');
+var myPath =  '/control.html?username=root&password=ZqGUJQen4KuvQJgbyrRGhYrbuMbXyKPV26zHLJmH&id=0&status=on';
+
+//The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
+var getOptions = {
+  host: 'arduino.com.br',
+  port: '5000',
+  path: '/control.html'
+};
+
+var postOptions = {
+  host: 'arduino.com.br',
+  path: myPath,
+  port: '5000',
+  method: 'POST'
+};
+
+callback = function(response) {
+  var str = '';
+
+  //another chunk of data has been recieved, so append it to `str`
+  response.on('data', function (chunk) {
+    str += chunk;
+  });
+
+  //the whole response has been recieved, so we just print it out here
+  response.on('end', function () {
+    ///str é a referencia para string de retorno do arduino
+    console.log(str);
+  });
+}
+http.request(getOptions, callback).end();
+
+
+
+//
+//
+function switchPower(id, status){
+  console.log("dispositivo "+ id + " status : "+status);
+  myPath =  '/control.html?username=root&password=ZqGUJQen4KuvQJgbyrRGhYrbuMbXyKPV26zHLJmH&id='+id+'&status='+status;
+  var postOptions = {
+  host: '192.168.2.28',
+  path: myPath,
+  port: '3000',
+  method: 'POST'
+};
+
+console.log("ligar disposito 00");
+http.request(postOptions, callback).end();
+};
 
 app.listen(9000);
 
