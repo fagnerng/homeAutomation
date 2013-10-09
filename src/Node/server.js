927fc8a39484bdc0fbc6582be1eb2c8a1c4ee6c5
Bruno Paiva <brunofpaiva>
2013-10-09 09:21:44 -0300

    servidor com merge

--- src/Node/server.js
+++ src/Node/server.js
@@ -4,11 +4,23 @@ var app = express();
 
 var users = [];
 var devices = [];
+var rootUser = false;
+var userDevices = [];
+var userLoged = false;
+var selectedUser;
+var iDs = [];
+
 
 var autLogin = ""
 console.log("Server Initiated");
 
 app.use(express.methodOverride());
+app.configure(function(){
+	app.use(express.static(__dirname + '/Paginas/img'));
+	console.log(__dirname + '/Paginas/img/');
+	console.log(__dirname + '/Paginas');
+});
+
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
@@ -32,27 +44,45 @@ function saveData(jsonData){
 		}
 	}); 	
 }
-
-function getNextUserID(){
+function changeStatus(id,status){
 	var jsonData = require('./BD/database.json');
-	var length = jsonData.alldata.users_length;
-	return length;
+	jsonDataBase = jsonData;
+	jsonDataBase.alldata.devices[id].status = status;
+
+	fs = require('fs');
+	var outputFilename = './BD/database.json';
+	console.log("in!");
+	fs.writeFile(outputFilename, JSON.stringify(jsonDataBase, null, 4), function(err) {
+		if(err) {
+		  console.log(err);
+		} else {
+		  console.log("JSON saved to ");
+		}
+	}); 
 }
 
-function createNewUser(user, deviceIDS){
+function removeUserByLogin(login){ // Trata para não remover o root (admin=true)
+	var jsonData = require('./BD/database.json');	
+	var index = findUserByLogin(login);
+	var usersBAK = jsonData.alldata.users;
+	jsonData.alldata.users = []
+	for ( i = 0;i<usersBAK.length;i++){
+		if( i!= index){
+			jsonData.alldata.users[i] = usersBAK[i];
+		}
+	}
+	saveData(jsonData);
+}
+
+function createNewUser(user){ //Adicionar os devices que ele vai ter
 	var jsonData = fillUsers();
-	var devs = "1"//deviceIDS.split("-"); 
 	
-	var idValue = getNextUserID();
-	console.log(idValue);
-	
-	jsonData.alldata.users_length = idValue+1;
-	jsonData.alldata.users[jsonData.alldata.users.length] = {	"id": idValue, 
+	jsonData.alldata.users[jsonData.alldata.users.length] = {
 												"name": user.name, 
 												"login": user.login, 
 												"pass": user.pass, 
 												"admin": "false", 
-												"devices": devs, 
+												"devices": user.devs, 
 												"email": user.email };
 	
 	saveData(jsonData);
@@ -63,19 +93,33 @@ function fillUsers(){
 	var usersLen = jsonData.alldata.users.length
 	
 	for (var i=0; i < usersLen; i++) {
-	    console.log(jsonData.alldata.users[i].name);
-	    users[i] = jsonData.alldata.users[i].name;
+		if (jsonData.alldata.users[i] != null){
+			users[i] = jsonData.alldata.users[i].name;
+		}
 	}
 	
 	return jsonData; 
 }
 
+function fillDevices(){
+	
+	var jsonData = require('./BD/database.json');
+	var devicesLen = jsonData.alldata.devices.length
+	
+	for (var i=0; i < devicesLen; i++) {
+	    console.log(jsonData.alldata.devices[i].name);
+	    devices[i] = jsonData.alldata.devices[i].name;
+	    userDevices[i] = i;
+	}
+}
 function findUserByLogin(login){
 	var jsonData = require('./BD/database.json');
 	var usersLen = jsonData.alldata.users.length
 	for (var i=0; i < usersLen; i++) {
-	    if ( login == jsonData.alldata.users[i].login){
-			return i;
+		if (jsonData.alldata.users[i] != null){
+			if ( login == jsonData.alldata.users[i].login){
+				return i;
+			}
 		}
 	}
 	return -1;
@@ -85,12 +129,61 @@ function findUserByEmail(email){
 	var jsonData = require('./BD/database.json');
 	var usersLen = jsonData.alldata.users.length
 	for (var i=0; i < usersLen; i++) {
-	    if ( email == jsonData.alldata.users[i].email){
-			return i;
+		if (jsonData.alldata.users[i] != null){
+			if ( email == jsonData.alldata.users[i].email){
+				return i;
+			}
 		}
 	}
 	return -1;
 }
+
+function getUserDevices(login){
+	var jsonData = require('./BD/database.json');
+	var indice = findUserByLogin(login);
+	console.log(indice);
+	var devicesUserLen = jsonData.alldata.users[indice].devices.length;
+	iDs = jsonData.alldata.users[indice].devices;
+	var devicesUser = [];
+	for(var i=0; i<devicesUserLen; i++){
+		devicesUser[i] =  jsonData.alldata.devices[jsonData.alldata.users[indice].devices[i]].name;
+	}
+	return devicesUser;
+}
+
+function allDevicesStatus(){
+	var devicesStatus = []
+	var jsonData = require('./BD/database.json');
+	var DevicesLen = jsonData.alldata.devices.length;
+
+	for (var i=0; i < userDevices.length; i++) {
+	    console.log(jsonData.alldata.devices[userDevices[i]].name);
+	    devicesStatus[i] = jsonData.alldata.devices[userDevices[i]].status;
+	}
+
+	return devicesStatus;
+}
+
+function switchPower(id, status){
+  var http = require('http');
+  console.log("dispositivo "+ id + " status : "+status);
+  var myPath =  '/control.html?username=root&password=ZqGUJQen4KuvQJgbyrRGhYrbuMbXyKPV26zHLJmH&id='+id+'&status='+status;
+  var postOptions = {
+  host: '192.168.2.28',
+  path: myPath,
+  port: '3000',
+  method: 'POST'
+	};
+
+	console.log("ligar disposito 00");
+	http.request(postOptions, console.log).end();
+};
+
+
+
+app.delete('/removeuser', function(req, res){
+	removeUserByLogin(req.param('login'));
+});
 
 app.get('/getusers', function(req, res){
 	fillUsers();
@@ -175,9 +268,9 @@ app.post('/adduser', function(req, res){
 		user.name = req.param('name');
 		user.login = req.param('login');
 		user.pass = req.param('pass');
-		devs = req.param('devs');
+		user.devs = req.param('devices').split("-");
 		user.email = req.param('email');
-		createNewUser(user, "1");
+		createNewUser(user);
 	}
 });
 
@@ -207,20 +300,37 @@ app.post('/checkLogin', function(req, res){
 	}
 });
 
-function switchPower(id, status){
-  var http = require('http');
-  console.log("dispositivo "+ id + " status : "+status);
-  var myPath =  '/control.html?username=root&password=ZqGUJQen4KuvQJgbyrRGhYrbuMbXyKPV26zHLJmH&id='+id+'&status='+status;
-  var postOptions = {
-  host: '192.168.2.28',
-  path: myPath,
-  port: '3000',
-  method: 'POST'
-	};
+app.post('/updatedevices', function(req, res){
+	var index = findUserByLogin(req.param('login'));
+	var jsonData = require('./BD/database.json');
+	var devs = req.param('devices').split("-");
+	console.log(req.param('devices'));
+	console.log("idnex", index);
+	
+	if (index != -1){
+		jsonData.alldata.users[index].devices = devs;
+		saveData(jsonData);
+	}
+});
 
-	console.log("ligar disposito 00");
-	http.request(postOptions, console.log).end();
-};
+app.post('/deleteDevices', function(req, res){
+	var devices = "";
+	var usuario = req.param('user'); // O usuario que tera os dispositivos selecionados
+	var dispositivos = req.param('selected'); // O id dos dispositivos a serem deletados
+});
+
+
+app.post('/deleteUser', function(req, res){
+	console.log(req.param('user'));
+	var usuario = req.param('user'); // O usuario que tera os dispositivos selecionados
+});
+
+
+app.post('/choicedUser', function(req, res){
+	login = req.param('login');
+	console.log(login+"aaaaaa");
+	choicedUser = getUserDevices(login);
+});
 
 app.listen(9000);
 
