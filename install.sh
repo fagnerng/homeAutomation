74b8d0f2b098cbc3cad537e390f0490a6da6b6ee
Fagner Gomes <fagner.gomes@ccc.ufcg.edu.br>
2013-10-25 12:48:02 -0300

    script de instalacao

---
+++ install.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/fagnerng/homeAutomation
+sudo apt-get install node
+cd  homeAutomation/src/server/
+node sv.js
+
--- src/Arduino/Testes/liteMain.lua
+++ src/Arduino/Testes/liteMain.lua
@@ -6,13 +6,14 @@ local failName = "O nome nao corresponde"
 local failPower = "O status do dispositivo nao corresponde"
 local failCode = "O Codigo de retorno nao corresponde"
 local failParans = "o numero de Parametros nao corresponde"
+
 --funcoes utilitarias
 local function getReqBody(ID, lPower)
 return "?username="..username.."&password="..password.."&status="..lPower .."&id="..ID
 end
 local function switchPower(ID, POWER)
 	local lPower
-	if POWER then lPower = "on" else lPower = "off" end
+	if POWER then lPower = POWER else lPower = "off" end
 	local rqBody = getReqBody(ID, lPower)
 	return TCP.POST( rqBody)
 end
@@ -34,20 +35,19 @@ end
 --funcao que cria todos os testes
 local function runTeste()
 local on,off = "on", "off"
--- seta desligados todos os dispositivos
-switchPower(0)
--- pega a instacia atual dos dispositivos
-local ventilador,codev = getDevice(0)
+
 
 --colecao de testes a serem realizados
 local colecaoTestes = {}
 	-- testfa a alteracao de estado dos dispositivo(liga desliga)
+	-- seta desligados todos os dispositivos
 	
+	--desliga e verifica
+	switchPower(0, off)
+	local ventilador,codev = getDevice(0)
 	ventilador, codev = getDevice(0)
 	colecaoTestes[1] = assert(off, ventilador.status,failPower)
-	
-	
-	--liga os dispositivos e atualiza as instancias do mesmo
+	--liga e verifica
 	switchPower(0,on)
 	ventilador, codev = getDevice(0)
 	colecaoTestes[2] = assert(on, ventilador.status,failPower)
@@ -56,37 +56,38 @@ local colecaoTestes = {}
 	--/*******************testa erros de autenticacao*********/
 	
 	
-	-- modificar o username para testar erro
+	
+	local a
+	
+	-- username invalido/inexistente
 	local luser = username
 	username = "odername"
-	local a
-	a, codev = switchPower(0)
+	a, codev = switchPower(0,off)
 	colecaoTestes[3] = assert(codev, "402",failCode)
 	-- teste de regressao, corrige o username para testar
 	username = luser
-	a, codev = switchPower(0)
+	a, codev = switchPower(0, off)
 	colecaoTestes[4] = assert(codev, "200",failCode)
 	
 	--/****testa se todos os parametros necessarios foram informado **/
 	-- muda o link de requisição para testar erro
+	
 	local mqreqbody = getReqBody
 	getReqBody = function() return "" end
-	a, codev = switchPower(0)
+	a, codev = switchPower(0, off)
 	colecaoTestes[5] = assert(codev, "403",failParans)
 	--teste de regressao
 	getReqBody = mqreqbody
-	a, codev = switchPower(0)
+	a, codev = switchPower(0, off)
 	colecaoTestes[6] = assert(codev, "200",failParans)
-	
+
 	--/************testa se o estado a ser setado é valido*********************/
-	getReqBody = function(id, power) 
-	return "?username="..username.."&password="..password.."&status=nada&id="..id
-	end
-	a, codev = switchPower(0)
+	local status = "invalido"
+	a, codev = switchPower(0, status)
 	colecaoTestes[7] = assert(codev, "405",failParans)
 	--teste de regressao
 	getReqBody = mqreqbody
-	a, codev = switchPower(0)
+	a, codev = switchPower(0, off)
 	colecaoTestes[8] = assert(codev, "200",failParans)
 	
 	
