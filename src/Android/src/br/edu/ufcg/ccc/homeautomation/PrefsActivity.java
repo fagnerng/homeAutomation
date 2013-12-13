3b351b1f3ffa53263170576312def51b909cb56a
Fagner Gomes <fagner.gomes@ccc.ufcg.edu.br>
2013-12-13 20:48:01 -0300

    remocao de preferencias

--- src/Android/src/br/edu/ufcg/ccc/homeautomation/PrefsActivity.java
+++ src/Android/src/br/edu/ufcg/ccc/homeautomation/PrefsActivity.java
@@ -1,7 +1,12 @@
 package br.edu.ufcg.ccc.homeautomation;
 
+import br.edu.ufcg.ccc.homeautomation.service.NotificationService;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+import android.view.View;
 
 public class PrefsActivity extends PreferenceActivity{
 	 
@@ -9,7 +14,23 @@ public class PrefsActivity extends PreferenceActivity{
 protected void onCreate(Bundle savedInstanceState) {
    super.onCreate(savedInstanceState);
    addPreferencesFromResource(R.xml.preference);
-   
+   findViewById(R.xml.preference).setOnClickListener(new View.OnClickListener() {
+	   SharedPreferences pref;
+	
+	@Override
+	public void onClick(View v) {
+		pref = PreferenceManager.getDefaultSharedPreferences(v.getContext());
+		boolean enable =pref.getBoolean("distance", true);
+		Intent i = new Intent(v.getContext().getApplicationContext(), NotificationService.class);
+		if (enable){
+			
+			startService(i);
+		}else{
+			stopService(i);
+		}
+		
+	}
+});
 }
 
 };
